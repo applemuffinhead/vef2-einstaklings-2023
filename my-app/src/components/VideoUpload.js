b81bbb3c4df36326905ee923688a23612eb7e7b5
applemuffinhead <hkv5@hi.is>
2023-04-21 22:14:47 +0000

    Er að reyna að laga uploadform

--- my-app/src/components/VideoUpload.js
+++ my-app/src/components/VideoUpload.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import "../styles/VideoUpload.css";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './VideoUpload.css';
 
 const VideoUpload = () => {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
   const [file, setFile] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
@@ -13,56 +13,63 @@ const VideoUpload = () => {
     setFile(e.target.files[0]);
   };
 
-  const API_URL =
-    "https://vef2-einstaklings-2023-production.up.railway.app/api";
+  const API_URL = "https://vef2-einstaklings-2023-production.up.railway.app/api";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     setLoading(true);
-
+  
     const formData = new FormData();
-    formData.append("title", title);
-    formData.append("description", description);
-    formData.append("file", file);
-
+    formData.append('title', title);
+    formData.append('description', description);
+    formData.append('file', file);
+  
     await fetch(`${API_URL}/videos/upload`, {
-      method: "POST",
+      method: 'POST',
       body: formData,
     });
-
+  
     setLoading(false);
-
-    navigate("/");
+  
+    navigate('/');
     window.location.reload();
   };
+  
 
   return (
-    <div>
-      <h1>Upload a new video</h1>
-      <form onSubmit={handleSubmit} className="upload-form">
-        <label>
-          Title:
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Description:
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </label>
-        <label>
-          Video file:
-          <input type="file" name="file" onChange={handleFileChange} required />
-        </label>
-        <button type="submit">{loading ? "Uploading..." : "Upload"}</button>
-      </form>
+    <div className="upload-container">
+      <div className="upload-form">
+        <h1>Upload a new video</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Title:</span>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            <span>Description:</span>
+            <textarea
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Video file:</span>
+            <input
+              type="file"
+              name="file"
+              onChange={handleFileChange}
+              required
+            />
+          </label>
+          <button type="submit">{loading ? 'Uploading...' : 'Upload'}</button>
+        </form>
+      </div>
     </div>
   );
 };
